7b52a4293de24a61616ddcf278cc86231eb361ba
MatheusDomingues <matheus_luige@hotmail.com>
2020-11-11 21:08:58 -0300

    floating button ok

---
+++ src/components/FloatingButton/FloatingButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link'
+
+import styles from './_floatingButton.module.css';
+
+type Props = {
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void,
+};
+
+const FloatingButton: React.FC<Props> = ({onClick}: Props) => {
+  return (
+    <div className={styles.plusContainer} onClick={onClick}>
+      <p>+</p>
+    </div>
+  );
+};
+
+export default FloatingButton;
