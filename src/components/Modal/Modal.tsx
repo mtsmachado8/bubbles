7a4426c19e64197d68a423d88a8d3040675ffde7
MatheusDomingues <matheus_luige@hotmail.com>
2020-12-02 19:20:16 -0300

    fix: TypeError

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,13 +4,13 @@ import styles from './_modal.module.css';
 
 type Props = {
   id?: string,
-  onClose: any,
+  onClose: (event: React.MouseEvent<HTMLParagraphElement, MouseEvent>) => void;
   children,
 }
 
 const Modal: React.FC<Props> = ({children, onClose, id = 'modal'}: Props) => {
   const handleOutsideClick = (e) => {
-    if(e.target.id === id) onClose();
+    if(e.target.id === id) onClose(e);
   }
 
   return (
